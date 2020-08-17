823e0a76bb662ee17f819e2ee4aea4e03779f1a6
Angelo Brandão <angelobms@gmail.com>
2020-08-17 08:32:27 -0300

    Added Note and Recipe models.

---
+++ src/main/java/com/bmsti/recipecatalogapp/domain/Note.java
@@ -0,0 +1,46 @@
+package com.bmsti.recipecatalogapp.domain;
+
+import javax.persistence.*;
+
+/**
+ * @author angelo santos
+ * @version 1.0
+ * @since 17/08/2020
+ */
+@Entity
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNote;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNote() {
+        return recipeNote;
+    }
+
+    public void setRecipeNote(String recipeNote) {
+        this.recipeNote = recipeNote;
+    }
+}
---
+++ src/main/java/com/bmsti/recipecatalogapp/domain/Recipe.java
@@ -0,0 +1,102 @@
+package com.bmsti.recipecatalogapp.domain;
+
+import javax.persistence.*;
+
+/**
+ * @author angelo santos
+ * @version 1.0
+ * @since 17/08/2020
+ */
+@Entity
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer servings;
+    private String source;
+    private String url;
+    //private Difficulty difficulty;
+
+    @Lob
+    private Byte[] image;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Note note;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPrepTime() {
+        return prepTime;
+    }
+
+    public void setPrepTime(Integer prepTime) {
+        this.prepTime = prepTime;
+    }
+
+    public Integer getCookTime() {
+        return cookTime;
+    }
+
+    public void setCookTime(Integer cookTime) {
+        this.cookTime = cookTime;
+    }
+
+    public Integer getServings() {
+        return servings;
+    }
+
+    public void setServings(Integer servings) {
+        this.servings = servings;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(Byte[] image) {
+        this.image = image;
+    }
+
+    public Note getNote() {
+        return note;
+    }
+
+    public void setNote(Note note) {
+        this.note = note;
+    }
+}
