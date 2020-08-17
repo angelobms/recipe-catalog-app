2a33c63930a96322f46de4ca63860386ff95b455
Angelo Brandão <angelobms@gmail.com>
2020-08-16 21:34:08 -0300

    Initial commit

---
+++ src/main/java/com/bmsti/recipecatalogapp/RecipeCatalogAppApplication.java
@@ -0,0 +1,13 @@
+package com.bmsti.recipecatalogapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeCatalogAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeCatalogAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bmsti/recipecatalogapp/RecipeCatalogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmsti.recipecatalogapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeCatalogAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
