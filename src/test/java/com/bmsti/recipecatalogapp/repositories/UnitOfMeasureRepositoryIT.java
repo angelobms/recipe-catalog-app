ce855b0a1bfce501ecd401dac44b995389d76671
Angelo Brandão <angelobms@gmail.com>
2020-08-30 19:40:30 -0300

    Renamed class integration test to using convention.

--- src/test/java/com/bmsti/recipecatalogapp/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/com/bmsti/recipecatalogapp/repositories/UnitOfMeasureRepositoryIT.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
-class UnitOfMeasureRepositoryIntegrationTest {
+class UnitOfMeasureRepositoryIT {
 
     @Autowired
     UnitOfMeasureRepository unitOfMeasureRepository;
