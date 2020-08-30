4f324bc521d517612788f3d06681900b214494bc
Angelo Brandão <angelobms@gmail.com>
2020-08-30 19:06:54 -0300

    Updated test unit controller Index for adding test unit with MockMVC.

--- src/test/java/com/bmsti/recipecatalogapp/controllers/IndexControllerTest.java
+++ src/test/java/com/bmsti/recipecatalogapp/controllers/IndexControllerTest.java
@@ -7,7 +7,13 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -15,6 +21,7 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+@AutoConfigureMockMvc
 class IndexControllerTest {
 
     @Mock
@@ -30,6 +37,16 @@ class IndexControllerTest {
         MockitoAnnotations.initMocks(this);
 
         indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    void tsetMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+
     }
 
     @Test
